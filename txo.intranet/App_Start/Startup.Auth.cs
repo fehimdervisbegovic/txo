df067c1e73afada30c0f2b52e157141c64f7a7dc
fehim <fehimdervisbegovic@gmail.com>
2016-07-20 22:44:13 +0200

    Identity moved to txo.entity layer. You should run Update-Database to create Identity entites. NuGet packages updated to most recent versions. Attention, this is seriuos commit...

--- txo.intranet/App_Start/Startup.Auth.cs
+++ txo.intranet/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using txo.intranet.Models;
+using txo.entities;
 
 namespace txo.intranet
 {
@@ -15,7 +16,7 @@ namespace txo.intranet
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext(IdentityDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
